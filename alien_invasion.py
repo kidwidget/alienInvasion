28c8f983564c329346e96aa95256cb325e3a39ac
kidwidget <kidwidget@gmail.com>
2024-03-01 22:49:31 -0500

    add bullets, set limit to 100 on screen at a time

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 #******************************************************************************
 class AlienInvasion:
@@ -18,6 +19,7 @@ class AlienInvasion:
 		
 		pygame.display.set_caption("Alien Invasion")
 		self.ship = Ship(self)
+		self.bullets = pygame.sprite.Group()
 		# Set the background color
 		self.bg_color = (230, 230, 230)
 
@@ -26,6 +28,8 @@ class AlienInvasion:
 		while True:
 			self._check_events()
 			self.ship.update()
+			self._update_bullets()
+				
 			self._update_screen()
 			self.clock.tick(60)
 			
@@ -48,6 +52,8 @@ class AlienInvasion:
 			self.ship.moving_left = True
 		elif event.key == pygame.K_q:
 			sys.exit()
+		elif event.key == pygame.K_SPACE:
+			self._fire_bullet()
 
 	def _check_keyup_events(self, event):
 		if event.key == pygame.K_RIGHT:
@@ -55,9 +61,28 @@ class AlienInvasion:
 		elif event.key == pygame.K_LEFT:
 			self.ship.moving_left = False
 
+	def _fire_bullet(self):
+		"""Create a new bullet and add it to the bullets group"""
+		if len(self.bullets) < self.settings.bullets_allowed:
+			new_bullet = Bullet(self)
+			self.bullets.add(new_bullet)
+	
+	def _update_bullets(self):
+		"""Update position of bullets an dget rid of old bullets."""
+		# Update bullet positions
+		self.bullets.update()
+		
+		# Get rid of bullets that have disappeared
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom <= 0:
+					self.bullets.remove(bullet)
+					# print(len(self.bullets))
+
 	def _update_screen(self):
 		# Redraw the screen during each passing of the loop
 		self.screen.fill(self.settings.bg_color)
+		for bullet in self.bullets.sprites():
+			bullet.draw_bullet()
 		self.ship.blitme()
 		# Make the moset recently drawn screen visible
 		pygame.display.flip()
