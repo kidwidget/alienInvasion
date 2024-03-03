053aeeeafe239a58ca9f3c98fe0b27116424cbb6
kidwidget <kidwidget@gmail.com>
2024-03-02 21:40:02 -0500

    added array of enemy ships

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 #******************************************************************************
 class AlienInvasion:
@@ -20,6 +21,9 @@ class AlienInvasion:
 		pygame.display.set_caption("Alien Invasion")
 		self.ship = Ship(self)
 		self.bullets = pygame.sprite.Group()
+		self.aliens  = pygame.sprite.Group()
+
+		self._create_fleet()
 		# Set the background color
 		self.bg_color = (230, 230, 230)
 
@@ -28,8 +32,7 @@ class AlienInvasion:
 		while True:
 			self._check_events()
 			self.ship.update()
-			self._update_bullets()
-				
+			self._update_bullets()				
 			self._update_screen()
 			self.clock.tick(60)
 			
@@ -84,9 +87,35 @@ class AlienInvasion:
 		for bullet in self.bullets.sprites():
 			bullet.draw_bullet()
 		self.ship.blitme()
+		self.aliens.draw(self.screen)
 		# Make the moset recently drawn screen visible
 		pygame.display.flip()
+	
+	def _create_fleet(self):
+		"""Create the fleet of aliens"""
+		# make an alein and keep adding aliens until there's no room left
+		# spacing between aliens is on alien width and one alien height
+		alien = Alien(self)
+		# self.aliens.add(alien)
+		alien_width, alien_height = alien.rect.size
 
+		current_x, current_y = alien_width, alien_height
+		while current_y < (self.settings.screen_height - 3 * alien_height):
+			while current_x < (self.settings.screen_width - 2 * alien_width):
+				self._create_alien(current_x, current_y)
+				current_x += 2 * alien_width
+			
+			# Finished a row; reset x value, increment y value.
+			current_x = alien_width
+			current_y += 2 * alien_height
+	
+	def _create_alien(self, x_position, y_position):
+		"""Create an alein and place it in the fleet"""
+		new_alien = Alien(self)
+		new_alien.x = x_position
+		new_alien.rect.x = x_position
+		new_alien.rect.y = y_position
+		self.aliens.add(new_alien)
 
 if __name__ == '__main__':
 
